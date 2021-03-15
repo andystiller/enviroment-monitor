83a45ff32a4389daf739938668e143f42bb5fe4d
Andy Stiller <andy@andyhat.co.uk>
2021-03-15 13:36:41 +0000

    Intial attempt at seperating sensons from display and config.

---
+++ config.py
@@ -0,0 +1,6 @@
+min_temp = None
+max_temp = None
+
+factor = 2.25
+HUE_MAX = 300
+HUE_MIN = 0
--- sensors.py
+++ sensors.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
-
-import time
 import os
 import sys
-import ST7735
-import logging
 
 try:
     # Transitional fix for breaking change in LTR559
@@ -17,3 +13,63 @@ from bme280 import BME280
 from enviroplus import gas
 from subprocess import PIPE, Popen
 
+# BME280 temperature/pressure/humidity sensor
+bme280 = BME280()
+
+def get_proximity():
+    proximity = ltr559.get_proximity()
+    return proximity
+
+# Get the temperature of the CPU for compensation
+def get_cpu_temperature():
+    process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, universal_newlines=True)
+    output, _error = process.communicate()
+    return float(output[output.index('=') + 1:output.rindex("'")])
+
+# Get the adjusted temperature
+def get_adjusted_temperature(factor):
+    cpu_temps = [get_cpu_temperature()] * 5
+    cpu_temp = get_cpu_temperature()
+    # Smooth out with some averaging to decrease jitter
+    cpu_temps = cpu_temps[1:] + [cpu_temp]
+    avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
+    raw_temp = bme280.get_temperature()
+    
+    if factor > 0:
+        adjusted_tmp = raw_temp - ((avg_cpu_temp - raw_temp) / factor)
+    else:
+        adjusted_tmp = raw_temp
+
+    return adjusted_tmp
+
+def get_pressure():
+    data = bme280.get_pressure()
+    return data
+
+def get_humidity():
+    data = bme280.get_humidity()
+    return data
+
+def get_light():
+    proximity = get_proximity()
+    
+    if proximity < 10:
+        data = ltr559.get_lux()
+    else:
+        data = 1
+    return data
+
+def get_oxidising():
+    data = gas.read_all()
+    data = data.oxidising / 1000
+    return data
+
+def get_reducing():
+    data = gas.read_all()
+    data = data.reducing / 1000
+    return data
+
+def get_nh3():
+    data = gas.read_all()
+    data = data.nh3 / 1000
+    return data
